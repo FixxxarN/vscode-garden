4b17041ea3ad6f1bd8835c9e45f7a344463217ff
FixxxarN <tornstromandre@gmail.com>
2023-06-11 14:13:42 +0200

    #2 Opening up a webview in the sidepanel

--- src/extension.js
+++ src/extension.js
@@ -1,34 +1,50 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
 function activate(context) {
+	const provider = new GardenProvider(context.extensionUri);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-garden" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-garden.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-garden!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.window.registerWebviewViewProvider(GardenProvider.viewType, provider));
 }
 
-// This method is called when your extension is deactivated
-function deactivate() {}
+class GardenProvider {
+	static viewType = 'vscodeGarden.view';
+
+	constructor(extensionUri) {
+		this._extensionUri = extensionUri;
+		this._view = undefined;
+	}
+
+	resolveWebviewView(webviewView, context, token) {
+		this._view = webviewView;
+
+		webviewView.webview.options = {
+			enableScripts: true,
+			localResourceRoots: [this._extensionUri]
+		}
+
+		webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+
+		webviewView.webview.onDidReceiveMessage(data => {
+			console.log(data);
+		})
+	}
+
+	_getHtmlForWebview(webview) {
+		return `<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Document</title>
+		</head>
+		<body>
+			<p>Hello, world</p>
+		</body>
+		</html>`
+	}
+}
+
+function deactivate() { }
 
 module.exports = {
 	activate,
